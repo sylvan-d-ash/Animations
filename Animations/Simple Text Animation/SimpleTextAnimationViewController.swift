58c9ab96aea792033166bad503b9b6a80764c45b
Sylvan Ash <sylvan.d.ash@gmail.com>
2019-09-16 13:12:13 +0300

    Fixed gesture + animated description label

--- Animations/Simple Text Animation/SimpleTextAnimationViewController.swift
+++ Animations/Simple Text Animation/SimpleTextAnimationViewController.swift
@@ -33,28 +33,56 @@ class SimpleTextAnimationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // title
+        self.title = NSLocalizedString("Simple Text Animation", comment: "Title for simple text animation view")
+
         // add gesture recognizer to initiate animation
-        self.view.addGestureRecognizer(UIGestureRecognizer(target: self, action: #selector(didTapView)))
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.didTapView)))
     }
 
 
     // MARK: - Animation
 
+    private var hasAnimating: Bool = false
 
-    @objc private func didTapView() {
+    @objc
+    private func didTapView() {
 
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+        // don't do anything if already animated
+        if self.hasAnimating { return }
+
+        // mark that we're animating
+        self.hasAnimating = true
+
+        // animate title label
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
             // move label to the left
             self.titleLabel.transform = CGAffineTransform(translationX: -30, y: 0)
 
         }) { (_) in
 
-            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseIn, animations: {
+            UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
                 // make label invisible
                 self.titleLabel.alpha = 0
 
                 // move label to the top and out of screen
-                self.titleLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: self.view.frame.height)
+                self.titleLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: -self.view.frame.height)
+            })
+        }
+
+        // animate description label with a slight delay
+        UIView.animate(withDuration: 0.5, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            // move label to the left
+            self.descriptionLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+
+        }) { (_) in
+
+            UIView.animate(withDuration: 1.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                // make label invisible
+                self.descriptionLabel.alpha = 0
+
+                // move label to the top and out of screen
+                self.descriptionLabel.transform = self.descriptionLabel.transform.translatedBy(x: 0, y: -self.view.frame.height)
             })
         }
     }
